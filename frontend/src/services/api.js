06e546b29c6ceb35b38e259cfc2fe5e0bccc2a15
Vanshika Aggarwal <vanshikaagg30@gmail.com>
2026-05-16 19:13:35 +0530

    frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,15 +1,7 @@
-// import axios from "axios";
-
-// const API = axios.create({
-//     baseURL: "https://ese-demo-candidate.onrender.com/api"
-// });
-
-// export default API;
-
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://ese-demo-candidate.onrender.com/api"
 });
 
 export default API;
